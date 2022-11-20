c4ca64d791061f0c90dbf361f3f4aaf977ef4f82
Eoloo <evans.oloo@student.moringaschool.com>
2022-11-20 16:27:38 +0300

    hover function added

--- index.js
+++ index.js
@@ -1,31 +1,32 @@
-$(document).ready(() =>{
+$(document).ready(() => {
 
 
-$(".images").click(() =>{
-    $("p").toggle();
-  });
+    $(".images").click(() => {
+        $("p").toggle();
+    });
 
-$("p").click(() =>{
-    $(".images").toggle();
-})
+    $("p").click(() => {
+        $(".images, h5").toggle();
+    })
 
-$(".imgs").hover(() =>{
-    $("img").mouseEnter()
-})
+    $(".imgs").hover(() => {
+        $("img").mouseEnter()
+        alert("mouse here")
+    })
 
-$('#form-group').click(() => {
-    const nameValue = $('.name form-control').val();
-    const emailValue = $('.email form-control').val();
-    const textAreaValue = $("#Message comment").val();
+    $('#form-group').click(() => {
+        const nameValue = $('.name form-control').val();
+        const emailValue = $('.email form-control').val();
+        const textAreaValue = $("#Message comment").val();
 
-    // Perform validation for input fields
-    let message = 'Please provide email and/or name'
-    if (nameValue == '' || emailValue == ''|| textAreaValue == '') message = 'Please provide email, some text here and/or name';
-    else { alert("Congratulations, we have received your message")}
+        // Perform validation for input fields
+        let message = 'Please provide email and/or name'
+        if (nameValue == '' || emailValue == '' || textAreaValue == '') message = 'Please provide email, some text here and/or name';
+        else { alert("Congratulations, we have received your message") }
 
-    // $('.Contactus').text(message);
+        // $('.Contactus').text(message);
 
-    
+
     })
 
     // alert(message);
