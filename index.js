3b2d7d260ce7573fe8fb12e893baeb81234dba48
Eoloo <evans.oloo@student.moringaschool.com>
2022-11-23 09:28:49 +0300

    debugging jquery

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-$(document).ready(() => {
+$(document).ready((event) => {
 
 
     $(".images").click(() => {
@@ -14,21 +14,20 @@ $(document).ready(() => {
         alert("mouse here")
     })
 
-    $('#form-group').click(() => {
+    $('.form-group').click(() => {
         const nameValue = $('.name form-control').val();
         const emailValue = $('.email form-control').val();
-        const textAreaValue = $("#Message comment").val();
+        const textAreaValue = $(".required form-control").val();
 
         // Perform validation for input fields
-        let message = 'Please provide email and/or name'
+        let message = 'Please provide email, some text here and/or name'
         if (nameValue == '' || emailValue == '' || textAreaValue == '') message = 'Please provide email, some text here and/or name';
         else { alert("Congratulations, we have received your message") }
 
-        // $('.Contactus').text(message);
 
 
     })
 
-    // alert(message);
+    event.preventDefault();
 
 })
