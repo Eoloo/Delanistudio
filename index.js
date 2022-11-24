c4f42166ff3c6058e5ac5dbb6280f0ba3d7e041e
Eoloo <evans.oloo@student.moringaschool.com>
2022-11-24 13:16:04 +0300

    indentation updated

--- index.js
+++ index.js
@@ -9,60 +9,60 @@ $(document).ready(() => {
         $(".images").toggle();
     })
 
-    $(".hover").hover(()=> {
-        $('.hover').css('opacity', '1' );
-    }, () =>{
-    $('.hover').css('opacity', '0');
+    $(".hover").hover(() => {
+        $('.hover').css('opacity', '1');
+    }, () => {
+        $('.hover').css('opacity', '0');
 
     });
-    $(".hover1").hover(()=> {
-        $('.hover1').css('opacity', '1' );
-    }, () =>{
-    $('.hover1').css('opacity', '0');
-
-    });
-
-    $(".hover2").hover(()=> {
-        $('.hover2').css('opacity', '1' );
-    }, () =>{
-    $('.hover2').css('opacity', '0');
+    $(".hover1").hover(() => {
+        $('.hover1').css('opacity', '1');
+    }, () => {
+        $('.hover1').css('opacity', '0');
 
     });
 
-    $(".hover3").hover(()=> {
-        $('.hover3').css('opacity', '1' );
-    }, () =>{
-    $('.hover3').css('opacity', '0');
+    $(".hover2").hover(() => {
+        $('.hover2').css('opacity', '1');
+    }, () => {
+        $('.hover2').css('opacity', '0');
 
     });
 
-    $(".hover4").hover(()=> {
-        $('.hover4').css('opacity', '1' );
-    }, () =>{
-    $('.hover4').css('opacity', '0');
+    $(".hover3").hover(() => {
+        $('.hover3').css('opacity', '1');
+    }, () => {
+        $('.hover3').css('opacity', '0');
 
     });
 
-    $(".hover5").hover(()=> {
-        $('.hover5').css('opacity', '1' );
-    }, () =>{
-    $('.hover5').css('opacity', '0');
+    $(".hover4").hover(() => {
+        $('.hover4').css('opacity', '1');
+    }, () => {
+        $('.hover4').css('opacity', '0');
 
     });
 
-    $(".hover6").hover(()=> {
-            $('.hover6').css('opacity', '1' );
-        }, () =>{
+    $(".hover5").hover(() => {
+        $('.hover5').css('opacity', '1');
+    }, () => {
+        $('.hover5').css('opacity', '0');
+
+    });
+
+    $(".hover6").hover(() => {
+        $('.hover6').css('opacity', '1');
+    }, () => {
         $('.hover6').css('opacity', '0');
 
     });
 
-    $(".hover7").hover(()=> {
-        $('.hover7').css('opacity', '1' );
-        }, () =>{
+    $(".hover7").hover(() => {
+        $('.hover7').css('opacity', '1');
+    }, () => {
         $('.hover7').css('opacity', '0');
 
-});
+    });
 
 
     $('#submit').click(() => {
@@ -71,16 +71,12 @@ $(document).ready(() => {
         let textAreaValue = $('#text').val();
 
         // Perform validation for input fields
-        if (nameValue == '' || emailValue == '' || textAreaValue == ''){
+        if (nameValue == '' || emailValue == '' || textAreaValue == '') {
             alert("Please input all the information")
         }
 
         else { alert("Congratulations, we have received your message") }
 
-
-
     })
-
-    // event.preventDefault();
 
 })
