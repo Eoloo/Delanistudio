757b62c26454e463e12767f8dd37b3a9602a42be
Eoloo <evans.oloo@student.moringaschool.com>
2022-11-24 13:08:57 +0300

    hover effect added

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-$(document).ready((event) => {
+$(document).ready(() => {
 
 
     $(".images").click(() => {
@@ -9,25 +9,78 @@ $(document).ready((event) => {
         $(".images").toggle();
     })
 
-    $(".imgs").hover(() => {
-        $("img").mouseEnter()
-        alert("mouse here")
-    })
+    $(".hover").hover(()=> {
+        $('.hover').css('opacity', '1' );
+    }, () =>{
+    $('.hover').css('opacity', '0');
 
-    $('#button').click(() => {
-        const nameValue = $('.name form-control').val();
-        const emailValue = $('.email form-control').val();
-        const textAreaValue = $(".required form-control").val();
+    });
+    $(".hover1").hover(()=> {
+        $('.hover1').css('opacity', '1' );
+    }, () =>{
+    $('.hover1').css('opacity', '0');
+
+    });
+
+    $(".hover2").hover(()=> {
+        $('.hover2').css('opacity', '1' );
+    }, () =>{
+    $('.hover2').css('opacity', '0');
+
+    });
+
+    $(".hover3").hover(()=> {
+        $('.hover3').css('opacity', '1' );
+    }, () =>{
+    $('.hover3').css('opacity', '0');
+
+    });
+
+    $(".hover4").hover(()=> {
+        $('.hover4').css('opacity', '1' );
+    }, () =>{
+    $('.hover4').css('opacity', '0');
+
+    });
+
+    $(".hover5").hover(()=> {
+        $('.hover5').css('opacity', '1' );
+    }, () =>{
+    $('.hover5').css('opacity', '0');
+
+    });
+
+    $(".hover6").hover(()=> {
+            $('.hover6').css('opacity', '1' );
+        }, () =>{
+        $('.hover6').css('opacity', '0');
+
+    });
+
+    $(".hover7").hover(()=> {
+        $('.hover7').css('opacity', '1' );
+        }, () =>{
+        $('.hover7').css('opacity', '0');
+
+});
+
+
+    $('#submit').click(() => {
+        let nameValue = $('#name').val();
+        let emailValue = $('#email').val();
+        let textAreaValue = $('#text').val();
 
         // Perform validation for input fields
-        let message = 'Please provide email, some text here and/or name'
-        if (nameValue == '' || emailValue == '' || textAreaValue == '') message = 'Please provide email, some text here and/or name';
+        if (nameValue == '' || emailValue == '' || textAreaValue == ''){
+            alert("Please input all the information")
+        }
+
         else { alert("Congratulations, we have received your message") }
 
 
 
     })
 
-    event.preventDefault();
+    // event.preventDefault();
 
 })
